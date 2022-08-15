0bc37d3f876534f292a00c01eb6b08ffe5ae3190
celyes <celyes02@gmail.com>
2022-08-15 16:24:11 +0100

    fix actions issue

--- tests/CommuneTest.php
+++ tests/CommuneTest.php
@@ -16,7 +16,9 @@ final class CommuneTest extends TestCase
     /** @test */
     public function if_commune_details_are_correct()
     {
-        $sampleCommune = Commune::where('name', 'Alger Centre')->first(['id', 'name', 'arabic_name', 'post_code', 'longitude', 'latitude']);
+        $sampleCommune = Commune::where('name', 'Alger Centre')
+            ->first(['id', 'name', 'arabic_name', 'post_code', 'longitude', 'latitude']);
+            
         $this->assertJsonStringEqualsJsonString(
             $sampleCommune->toJson(),
             '{"id":554,"name":"Alger Centre","arabic_name":"الجزائر الوسطى","longitude":3.0612244,"latitude":36.7712246,"post_code": "16001"}'
